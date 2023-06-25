60c769416a26a96ddfa3d570134c935c5d0f7f4a
iroda0103 <iroda3242@gmail.com>
2023-06-25 11:51:19 +0500

    users va brendni sintaksisini to'g'irla

--- migrations/20230624081708_users.js
+++ migrations/20230624081708_users.js
@@ -4,12 +4,12 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable("users", (table) => {
-    table.increments(id).primary(),
-      table.string("first_name").notNullable(),
-      table.string("last_name").notNullable(),
-      table.string("username").notNullable().unique(),
-      table.string("password").notNullable(),
-      table.enum("role", ["admin"]).notNullable();
+    table.increments(id).primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("username").notNullable().unique();
+    table.string("password").notNullable();
+    table.enum("role", ["admin"]).notNullable();
   });
 };
 
--- migrations/20230625034634_brends.js
+++ migrations/20230625034634_brends.js
@@ -2,17 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return knex.schema.createTable('brends',(table)=>{
-    table.increments('id').primary(),
-    table.string('name').unique()
-  })
+exports.up = function (knex) {
+  return knex.schema.createTable("brends", (table) => {
+    table.increments("id").primary();
+    table.string("name").unique();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  return knex.schema.dropTable('brends')
+exports.down = function (knex) {
+  return knex.schema.dropTable("brends");
 };
