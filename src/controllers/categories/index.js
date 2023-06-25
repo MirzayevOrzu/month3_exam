a7fd1dfbfa3290405501da22fa34167835710c8b
iroda0103 <iroda3242@gmail.com>
2023-06-25 18:01:49 +0500

    category uchun router qo'sh

--- src/controllers/categories/index.js
+++ src/controllers/categories/index.js
@@ -9,11 +9,11 @@ const db = require("../../db");
 const postCategory = async (req, res) => {
   try {
     const { name } = req.body;
-    const a = await db("categories")
-    
+
+    const category = await db("categories").insert(req.body).returning("*");
+
     res.status(200).json({
-      mm: name,
-      a
+      category,
     });
   } catch (error) {
     res.status(400).json({
@@ -22,6 +22,107 @@ const postCategory = async (req, res) => {
   }
 };
 
+/**
+ * Category qo'shish uchun yo'l
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @returns
+ */
+const getCategory = async (req, res) => {
+  try {
+    let { order_by='id', q } = req.query;
+    let { limit = 4, offset = 0 } = req.body;
+
+      order_by = order_by.split("-");
+
+    let dbQuery = db("categories").select("id", "name");
+
+    if (q) {
+      dbQuery.andWhereILike("categories.name", `%${q}%`);
+    }
+
+    const total = await dbQuery.clone().count().groupBy("id");
+
+    dbQuery.orderBy(...order_by);
+    dbQuery.limit(limit).offset(offset);
+
+    const categories = await dbQuery;
+
+    res.status(200).json({
+      data: categories,
+      pageInfo: {
+        total: total.length,
+        limit,
+        offset,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      err: err.message,
+    });
+  }
+};
+
+/**
+ * Category o'chirish uchun yo'l
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @returns
+ */
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const existing = await db('categories').where({ id }).first();
+
+    if (!existing) {
+      return res.status(404).json({
+        error: `${id}-idle category topilmadi`,
+      });
+    }
+
+    const deleted = await db('categories')
+      .where({ id })
+      .delete()
+      .returning(['id', 'name']);
+
+    res.status(200).json({
+      deleted: deleted[0],
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * Category tahrirlash uchun yo'l
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @returns
+ */
+const patchCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updated = await db('categories').where({ id }).update(req.body).returning('*');
+
+    res.status(200).json({
+      updated: updated[0],
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+
+
 module.exports = {
   postCategory,
+  getCategory,
+  deleteCategory,
+  patchCategory
 };
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const Validator = require("../shared/validator");
-const { postCategory } = require("../controllers/categories");
+const { postCategory,getCategory ,deleteCategory,patchCategory} = require("../controllers/categories");
 const { patchSchema,postSchema } = require("../controllers/categories/schemas");
 
 const router = express.Router();
 
 router.post("/categories", Validator(postSchema), postCategory);
-
+router.get('/categories',getCategory)
+router.delete('/categories/:id',deleteCategory)
+router.patch('/categories/:id',patchCategory)
 module.exports = router;
