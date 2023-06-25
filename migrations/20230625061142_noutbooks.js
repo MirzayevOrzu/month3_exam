e3004e68dcd52fccf9cafba33522011f07670246
iroda0103 <iroda3242@gmail.com>
2023-06-25 11:49:11 +0500

    noutbooklar uchun migration qo'sh

---
+++ migrations/20230625061142_noutbooks.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('noutbooks',(table)=>{
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.integer('price');
+    table.string('description');
+    table.integer('model_id').references('id').inTable('models').onDelete('CASCADE');
+    table.integer('category_id').references('id').inTable('categories').onDelete('SET NULL');
+    table.string('protsessor').notNullable();
+    table.string('xotirasi').notNullable();
+    table.string('ekran_diagonali');
+    table.string('rangi');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('noutbooks')
+};
