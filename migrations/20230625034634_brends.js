a0b20eb224bf5443d6ee3c04e2dc38eadae85c79
iroda0103 <iroda3242@gmail.com>
2023-06-25 10:27:37 +0500

    brends uchun migration qo'sh

---
+++ migrations/20230625034634_brends.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('brends',(table)=>{
+    table.increments('id').primary(),
+    table.string('name').unique()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('brends')
+};
