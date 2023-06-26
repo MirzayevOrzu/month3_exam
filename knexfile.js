91f2343e3722f6f3204540616428afa11b42e205
iroda0103 <iroda3242@gmail.com>
2023-06-26 07:37:25 +0500

    configlarni tahrirla

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
+      host:config.db.host,
       database: config.db.name,
       user:  config.db.user,
       password: config.db.password
--- src/shared/config/index.js
+++ src/shared/config/index.js
@@ -4,6 +4,7 @@ dotenv.config()
 const config = {
   port: process.env.PORT,
   db: {
+    host:process.env.DB_HOST,
     port: process.env.DB_PORT,
     name: process.env.DB_NAME,
     user: process.env.DB_USER,
