b993d26875f3df9639076b782ed8aece92d7e22b
iroda0103 <iroda3242@gmail.com>
2023-06-26 07:30:36 +0500

    model uchun migrationni qo'sh

--- migrations/20230625052802_models.js
+++ migrations/20230625052802_models.js
@@ -6,6 +6,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("models", (table) => {
     table.increments("id").primary();
     table.string("name").unique();
+    table.string("images");
     table
       .integer("brand_id")
       .references("id")
